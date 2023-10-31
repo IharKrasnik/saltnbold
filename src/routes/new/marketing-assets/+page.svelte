00947eca80bc45809c3b910d74494b21504566ad
Igor Krasnik <ihar.krasnik@gmail.com>
2023-10-31 21:38:51 +0100

    Marketing portfolio

--- src/routes/new/marketing-assets/+page.svelte
+++ src/routes/new/marketing-assets/+page.svelte
@@ -5,6 +5,7 @@
 	import currentUser, { isLoading as isCurrentUserLoading } from '$lib/stores/currentUser';
 	import Form from '$lib/components/Form.svelte';
 	import LoginButton from '$lib/components/LoginButton.svelte';
+	import RenderUrl from '$lib/components/RenderUrl.svelte';
 	import requests, { createRequest } from '$lib/stores/requests';
 	import { goto } from '$app/navigation';
 
@@ -57,4 +58,39 @@
 			<LoginButton />
 		</div>
 	{/if}
+</div>
+
+<div class="my-12">
+	<div class="section-header">
+		<div>
+			<h2>Marketing assets examples</h2>
+			<h3 class="mt-2">Here's what we've created for founders like you</h3>
+		</div>
+	</div>
+	<div class="section-body grid sm:grid-cols-2 gap-4">
+		<RenderUrl
+			url="https://cdn.dribbble.com/userupload/8991636/file/original-ce8aa57c21e12a834b2aad762fe5b1ad.png?resize=1504x1128"
+		/>
+
+		<RenderUrl
+			url="https://ship-app-assets.fra1.digitaloceanspaces.com/stream/rec4sLfwGXzHxLy54/1698783852535-image.png"
+		/>
+		<RenderUrl url="https://pbs.twimg.com/media/F7v8eInWgAAVf-K?format=jpg&name=large" />
+
+		<RenderUrl
+			url="https://cdn.dribbble.com/userupload/10800002/file/original-cddf211e48faa78a9df60d4448363d32.png?resize=2048x1536"
+		/>
+		<RenderUrl
+			url="https://cdn.dribbble.com/userupload/9989970/file/original-918b309aa9d6c9c5f01c76837cc5cd3a.png?resize=2048x1536"
+		/>
+		<RenderUrl
+			url="https://cdn.dribbble.com/userupload/8137499/file/original-4da17a928f891ea4f57fb8a43599a0ee.jpg?crop=361x0-2598x1678&resize=1000x750&vertical=center"
+		/>
+		<RenderUrl
+			url="https://blog.paralect.com/storage/app/uploads/public/5e2/06d/2e9/5e206d2e9bcc9372946966.png"
+		/>
+		<RenderUrl
+			url="https://blog.paralect.com/storage/app/uploads/public/63a/1b1/379/63a1b1379a685165502527.png"
+		/>
+	</div>
 </div>
